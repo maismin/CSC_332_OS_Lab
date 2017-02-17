5be98d54a2ce41f26f1c0765c8d153762e58a2ec
Simon Mai <maismin@gmail.com>
2017-02-17 09:21:56 -0500

    Started lab1 hw

---
+++ Task1/displaycontent.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+// for open command
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>     // for read/write
+
+#define BUFF_LENGTH 128
+
+int main (int argc, char* argv[]) {
+    int fd;
+    
+    char *filename = argv[1];
+
+    errno = 0;          // indicates what error occurred
+
+    fd = open(filename, O_RDONLY);
+
+    if (fd < 0) { 
+        perror("open");
+        return 1;
+    }   
+    
+    char buff[BUFF_LENGTH];
+
+    ssize_t length;
+
+    while ((length = read(fd, buff, BUFF_LENGTH)) != 0) {
+        write(1, buff, length);
+    }
+
+    close(fd);
+
+    return 0;
+}
